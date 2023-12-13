d988988eb51143a92dbf239739a9a4c7f9bf76fc
Bafod <141050501+Bafod@users.noreply.github.com>
2023-12-13 18:09:15 -0300

    Teste

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,5 +1,12 @@
 package br.com.alura.screenmatch.model;
 
-record dadoSerie() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonAlias("Released") String dataLancamento) {
 
 }
